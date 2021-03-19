bb03d0acaee218e74e11af15eb8af0e7a2e56dff
Jonas Wyss <jonaswyss@gmail.com>
2021-03-19 08:04:11 +0000

    drive again

--- src/remote/app.py
+++ src/remote/app.py
@@ -7,20 +7,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-switch.init()
+# switch.init()
 @socketio.on('drive')
 def handle_drive_instruction(data:Dict):
-    pass
-    # print('Drive')
-    # print(data)
-    # if 'direction' in data.keys():
-    #     direction:int = data['direction']
-    #     if direction == 0:
-    #         driver.stop()
-    #     else:
-    #         driver.drive(direction)
+    print('Drive')
+    print(data)
+    if 'direction' in data.keys():
+        direction:int = data['direction']
+        if direction == 0:
+            driver.stop()
+        else:
+            driver.drive(direction)
 
-    # driver.drive()
+    driver.drive()
 
 if __name__ == '__main__':
     socketio.run(app, port=8988)
