91015892108e8ae943b8f1ba6f6d90baf2c5524d
Jonas Wyss <jonaswyss@gmail.com>
2021-04-29 10:44:15 +0200

    fix client

--- oivonen-web/src/app/app.module.ts
+++ oivonen-web/src/app/app.module.ts
@@ -4,10 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RemoteComponent } from './remote/remote.component';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { NgxGamepadModule } from 'ngx-gamepad';
 
-const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
 
 @NgModule({
   declarations: [
@@ -17,7 +15,6 @@ const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SocketIoModule.forRoot(config),
     NgxGamepadModule,
   ],
   providers: [],
--- oivonen-web/src/app/remote-control.service.ts
+++ oivonen-web/src/app/remote-control.service.ts
@@ -1,31 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
+import { io, Socket } from "socket.io-client";
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RemoteControlService {
+  private socket: Socket = this.createSocket('http://localhost:8988');
+  constructor() {
 
-  constructor(private socket: Socket) { }
+  }
 
   sendDriveCommand(data: DriveInstructions) {
     console.log(data)
     this.socket.emit("drive", data);
   }
 
-  sendSpeedCommand(speedDelta:number){
+  sendSpeedCommand(speedDelta: number) {
     this.socket.emit("speed", speedDelta)
   }
 
   sendMessage(msg: string) {
     this.socket.emit("message", msg);
   }
-  getMessage() {
-    return this.socket
-      .fromEvent("message")
-      .pipe(map((data: any) => data.msg));
+
+  createSocket(address:string)
+  {
+    return io(address, {host:"*"})
   }
+
+  setAddress(address: string) {
+    this.socket.close()
+    this.socket = this.createSocket(address);
+  }
+
 }
 
 export interface DriveInstructions {
