e928ffbabeade173640497116cca935e81e3687f
Patrick Heim <patrick.heim@stud.hslu.ch>
2021-06-08 11:16:51 +0200

    implement get_position()

--- src/getposition.py
+++ src/getposition.py
@@ -3,5 +3,23 @@ from typing import List
 
 
 def get_position(measuredDistanceLeft:float, measuredDistanceRight:float, obstaclesOfCurrentStep:List[bool]):
-    # TODO
-    return measuredDistanceLeft
+    stairWidth = 136
+    cameraOffset = 11
+    possiblePositionFromLeftSensor = 0
+    possiblePositionFromRightSensor = 0
+    possiblePositionAverage = 0
+    if(measuredDistanceLeft < 40 and measuredDistanceLeft > 0):
+        possiblePositionFromLeftSensor = min(measuredDistanceLeft + cameraOffset, stairWidth - cameraOffset)
+
+    if(measuredDistanceRight < 40 and measuredDistanceRight > 0):
+        possiblePositionFromRightSensor = max(stairWidth - measuredDistanceRight - cameraOffset, cameraOffset)
+
+    if(possiblePositionFromLeftSensor > 0):
+        if(possiblePositionFromRightSensor > 0):
+            possiblePositionAverage = (possiblePositionFromLeftSensor + possiblePositionFromRightSensor) / 2
+        else:
+            possiblePositionAverage = possiblePositionFromLeftSensor
+    elif(possiblePositionFromRightSensor > 0):
+        possiblePositionAverage = possiblePositionFromRightSensor
+
+    return possiblePositionAverage
